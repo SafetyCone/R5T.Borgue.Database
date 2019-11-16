eb2e79141a8d8595df431dcc1033606444dde8e6
MinexAutomation <minex.automation@gmail.com>
2019-11-16 14:01:51 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Borgue.Database.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Borgue.Database.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
