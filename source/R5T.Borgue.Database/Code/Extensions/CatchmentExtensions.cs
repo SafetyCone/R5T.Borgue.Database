0734714e3ca0f68c0611a562a4c36ee96a9caaea
MinexAutomation <minex.automation@gmail.com>
2019-12-30 01:30:50 -0800

    Modified catchment extensions to use Geometry and LngLat extensions.

--- source/R5T.Borgue.Database/Code/Extensions/CatchmentExtensions.cs
+++ source/R5T.Borgue.Database/Code/Extensions/CatchmentExtensions.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 
-using GeoAPI.Geometries;
-using NetTopologySuite;
-using NetTopologySuite.Geometries;
-
 using R5T.Corcyra;
 
 using AppType = R5T.Corcyra.Catchment;
@@ -22,43 +18,22 @@ namespace R5T.Borgue.Database
                 Name = entityType.Name,
             };
 
-            foreach (var coordinate in entityType.Boundary.Coordinates)
-            {
-                appType.Boundary.Add(new LngLat() { Lng = coordinate.X, Lat = coordinate.Y }); // Is this right? X = Longitude, Y = Latitude. This is recommended by: https://docs.microsoft.com/en-us/ef/core/modeling/spatial
-            }
+            var lngLats = entityType.Boundary.ToLngLats();
+
+            appType.Boundary.AddRange(lngLats);
 
             return appType;
         }
 
         public static EntityType ToEntityType(this AppType appType)
         {
-            var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326); // Recommended SRID, used by Google Maps.
-
-            int numCoordinates = appType.Boundary.Count;
-            var coordinates = new Coordinate[numCoordinates + 1];
-            for (int iCoordinate = 0; iCoordinate < numCoordinates; iCoordinate++)
-            {
-                var vertex = appType.Boundary[iCoordinate];
-
-                var coordinate = new Coordinate(vertex.Lng, vertex.Lat);
-
-                coordinates[iCoordinate] = coordinate;
-            }
-
-            // Make sure ring is closed (first point equals last point).
-            var firstVertex = appType.Boundary[0];
-
-            coordinates[numCoordinates] = new Coordinate(firstVertex.Lng, firstVertex.Lat);
-
-            var linearRing = new LinearRing(coordinates);
-
-            var geometry = new Polygon(linearRing, geometryFactory);
+            var polygon = appType.Boundary.ToPolygon();
 
             var entity = new EntityType()
             {
                 Identity = appType.Identity.Value,
                 Name = appType.Name,
-                Boundary = geometry,
+                Boundary = polygon,
             };
 
             return entity;
